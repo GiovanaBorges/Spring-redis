e51973a72ce8ffa6da1f2ec99400e2bf9995e19b
giovana borges <gigi12345as17@gmail.com>
2025-11-03 21:24:18 -0300

    refactor: change Product ID type for MongoDB integration

--- src/main/java/com/redis/redis/models/Product.java
+++ src/main/java/com/redis/redis/models/Product.java
@@ -1,16 +1,22 @@
 package com.redis.redis.models;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Data
+@Document(collection = "products")
 public class Product{
-    private Long id;
+    @Id
+    private String id;
     private String name;
     private Double price;
 }
